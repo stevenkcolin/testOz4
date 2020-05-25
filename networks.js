9ada2d2d666f030c58969b380a5c8a5bb565e098
stevenkcolin <steven.k.colin@gmail.com>
2020-05-25 08:38:04 +0800

    submit

--- networks.js
+++ networks.js
@@ -3,14 +3,6 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   networks: {
-    test: {
-      protocol: 'http',
-      host: '127.0.0.1',
-      port: 8545,
-      gas: 5000000,
-      gasPrice: 5e9,
-      networkId: '*',
-    },
     development: {
       protocol: 'http',
       host: '127.0.0.1',
